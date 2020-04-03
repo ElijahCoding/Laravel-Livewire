07b43d3758c410414bb94cfce33e1ba74004728d
ElijahCoding <cuiyeqing960904@gmail.com>
2020-04-03 18:03:38 +0300

    validation

--- resources/views/livewire/post-create.blade.php
+++ resources/views/livewire/post-create.blade.php
@@ -2,7 +2,13 @@
     <form wire:submit.prevent="addPost">
         <div class="form-group">
             <label for="body" class="sr-only">Your post</label>
-            <textarea name="body" id="body" class="form-control" wire:model="body"></textarea>
+            <textarea name="body" id="body" class="form-control" wire:model="body @error('body') is-invalid @enderror"></textarea>
+
+            @error('body')
+                <span class="invalid-feedback">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
         </div>
 
         <button class="btn btn-primary" type="submit">
