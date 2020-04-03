4e88025bab1eb2c53c5e2944cc51c292df439997
ElijahCoding <cuiyeqing960904@gmail.com>
2020-04-03 16:56:54 +0300

    link function to php

--- app/Http/Livewire/PostCreate.php
+++ app/Http/Livewire/PostCreate.php
@@ -6,6 +6,13 @@ use Livewire\Component;
 
 class PostCreate extends Component
 {
+    public $body;
+
+    public function addPost()
+    {
+        dd('working');
+    }
+
     public function render()
     {
         return view('livewire.post-create');
--- resources/views/livewire/post-create.blade.php
+++ resources/views/livewire/post-create.blade.php
@@ -1,3 +1,12 @@
-<div>
-    {{-- Do your work, then step back. --}}
+<div class="mb-4">
+    <form wire:submit.prevent="addPost">
+        <div class="form-group">
+            <label for="body" class="sr-only">Your post</label>
+            <textarea name="body" id="body" class="form-control" wire:model="body"></textarea>
+        </div>
+
+        <button class="btn btn-primary" type="submit">
+            Post
+        </button>
+    </form>
 </div>
